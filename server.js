55bf7c67b961f4fc1d951773f9807a3d659bbe4d
jsk3342 <kss2460@naver.com>
2022-09-13 20:00:30 +0900

    Add: server 구현

--- server.js
+++ server.js
@@ -1,15 +1,49 @@
 const { application } = require("express");
 const express = require("express");
 const app = express();
+app.use(express.urlencoded({ extended: true }));
+const MongoClient = require("mongodb").MongoClient;
+app.set("view engine", "ejs");
 
-app.listen(8080, function () {
-  console.log("on 8080");
+var db;
+MongoClient.connect(
+  "mongodb+srv://admin:qwer1234@cluster0.71rse6y.mongodb.net/todoapp?retryWrites=true&w=majority",
+  { useUnifiedTopology: true },
+  function (에러, client) {
+    if (에러) return console.log(에러);
+    db = client.db("todoapp");
+
+    db.collection("post").insertOne(
+      { 이름: "John", _id: 100 },
+      function (에러, 결과) {
+        console.log("저장완료");
+      }
+    );
+
+    app.listen(8080, function () {
+      console.log("on 8080");
+    });
+  }
+);
+
+app.get("/write", function (req, res) {
+  res.sendFile(__dirname + "/write.html");
 });
 
-app.get("/pet", function (요청, 응답) {
-  응답.send("펫 용품 페이지");
+app.get("/", function (req, res) {
+  res.sendFile(__dirname + "/index.html");
 });
 
-app.get("/beauty", function (req, res) {
-  res.send("뷰티 용품 페이지");
+app.post("/add", (요청, 응답) => {
+  db.collection("post").insertOne(
+    { title: 요청.body.title, date: 요청.body.date },
+    function (에러, 결과) {
+      console.log("add 저장완료");
+    }
+  );
+  응답.send("전송완료");
+});
+
+app.get("/list", (req, res) => {
+  res.render("list.ejs");
 });
